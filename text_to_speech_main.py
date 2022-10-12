8c1eae610e83e46988d52475bbe6619377e89cb7
TechVine <115523060+TechVine@users.noreply.github.com>
2022-10-12 16:15:55 +0530

    Create text_to_speech_main.py

---
+++ text_to_speech_main.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[0].id)
+
+x = 'Hello Everyone I am Contributing in Hacktoberfest'
+
+
+engine.say(x)
+
+engine.runAndWait()
+
+
+
+
